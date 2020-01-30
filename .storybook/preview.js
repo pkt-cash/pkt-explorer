1dfb54e70fa71532ba090f11af8c8daf2fdb82fb
gorhgorh <gorhgorh@lesnodebots.eu>
2020-01-30 15:47:58 +0100

    add blockList stories, add router to storybook decorator

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { addDecorator, configure } from '@storybook/react';
 import GlobalStyles from '../src/App.css.js'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import theme from '../src/theme/theme'
 addDecorator(storyFn => <><GlobalStyles />
-  <ThemeProvider theme={theme}>
-    {storyFn()}
-  </ThemeProvider>
+  <Router>
+    <ThemeProvider theme={theme}>
+      {storyFn()}
+    </ThemeProvider>
+  </Router>
 </>)
 configure(require.context('../src/components', true, /\.stories\.js$/), module)
---
+++ src/components/BlockList/BlockList.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+
+const BlockListCont = styled.div`
+
+`
+
+const BlockList = ({ listData }) => {
+  return (
+    listData
+      ? <BlockListCont>
+          {listData.results.map((blk) => <div key={blk.hash}>{blk.height}</div>)}
+      </BlockListCont>
+      : <div>loading</div>
+  )
+}
+
+BlockList.propTypes = {
+  listData: PropTypes.array
+}
+
+BlockList.defaultProps = {
+
+}
+
+export default BlockList
