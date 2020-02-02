3ed2e903a191aa09d9e3d26502cf320bc94014fd
Thierry Marianne <thierry@marianne.io>
2020-02-02 15:52:53 +0100

    Revise style of rich list

--- src/components/RichList/RichList.js
+++ src/components/RichList/RichList.js
@@ -9,14 +9,18 @@ const RichListCell = styled.div`
   width: 100%;
 `
 
-const RichListHeightCell = styled(RichListCell)`
-  justify-content: flex-start;
+const RichListAddressCell = styled(RichListCell)`
   cursor: pointer;
-  color: ${({ theme }) => theme.colors.pktBlueLight} 
+  justify-content: flex-start;
+  color: ${({ theme }) => theme.colors.pktBlueLight};
+  text-decoration: underline;
 `
 
 const RichListCont = styled.div`
-  margin: ${metrics.margin}px;
+  border-top: solid 1px ${({ theme }) => theme.colors.pktGreyLight};
+  border-right: solid 1px ${({ theme }) => theme.colors.pktGreyLight};
+  box-shadow: -2px 2px 2px ${({ theme }) => theme.colors.pktGreyLight};
+  margin: ${metrics.margin}rem;
 `
 
 const RichListLabel = styled.div`
@@ -33,15 +37,19 @@ const RichListLabel = styled.div`
 
 const RichListLabelsCont = styled.div`
   display: flex;
+  border-bottom: solid 1px ${({ theme }) => theme.colors.pktGreyLight};
   justify-content: space-between;
-  margin-bottom: ${metrics.margin}px;
+  padding: ${metrics.padding}rem;
   text-transform: capitalize;
   width: 100%;
 `
 const RichListRow = styled.div`
-  display: flex;
   align-items: flex-end;
+  align-items: center;
+  display: flex;
+  height: ${metrics.rowHeight}rem;
   justify-content: space-between;
+  padding: 0 ${metrics.padding}rem;
   width: 100%;
   
   :nth-child(2n + 1) {
@@ -70,9 +78,9 @@ const RichList = ({ listData }) => {
           {/* Mapping over cells for each address  */}
           {Object.values(cells).map((cellName) => {
             if (cellName === 'address') {
-              return <RichListHeightCell key={`${address.address}-${cellName}`}>
+              return <RichListAddressCell key={`${address.address}-${cellName}`}>
                 {address[cellName]}
-              </RichListHeightCell>
+              </RichListAddressCell>
             }
 
             return <RichListCell key={`${address.address}-${cellName}`}>
