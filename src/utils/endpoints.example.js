9b513fc2725c0af61c456776af5aa24ee47b342f
gorhgorh <gorhgorh@lesnodebots.eu>
2020-03-11 15:49:51 +0100

    cleanup merge

--- src/utils/endpoints.example.js
+++
@@ -1,9 +0,0 @@
-const backendURL = process.env.REACT_APP_BACKEND_URL || `https://pkt.cash`
-export default {
-  blkLApi: `${backendURL}/api/v1/PKT/pkt/chain/down`,
-  richLApi: `${backendURL}/api/v1/PKT/pkt/stats/richlist`,
-  addrMetaApi: `${backendURL}/api/v1/PKT/pkt/address`,
-  blockApi: `${backendURL}/api/v1/PKT/pkt/block`,
-  pkApi: `${backendURL}/api/v1/PKT/pkt/packetcrypt/stats`,
-  txStats: `${backendURL}/api/v1/PKT/pkt/stats/daily-transactions`
-}
--- src/utils/endpoints.js
+++ src/utils/endpoints.js
@@ -1,8 +1,9 @@
+const backendURL = process.env.REACT_APP_BACKEND_URL || 'https://pkt.cash'
 export default {
-  blkLApi: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/chain/down',
-  richLApi: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/stats/richlist',
-  addrMetaApi: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/address',
-  blockApi: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/block',
-  pkApi: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/packetcrypt/stats',
-  txStats: 'https://alpha-pkt-explorer.cjdns.fr/api/v1/PKT/pkt/stats/daily-transactions'
+  blkLApi: `${backendURL}/api/v1/PKT/pkt/chain/down`,
+  richLApi: `${backendURL}/api/v1/PKT/pkt/stats/richlist`,
+  addrMetaApi: `${backendURL}/api/v1/PKT/pkt/address`,
+  blockApi: `${backendURL}/api/v1/PKT/pkt/block`,
+  pkApi: `${backendURL}/api/v1/PKT/pkt/packetcrypt/stats`,
+  txStats: `${backendURL}/api/v1/PKT/pkt/stats/daily-transactions`
 }
