ebb8fc3fa25602dda7532503838546aaa0562f8a
Caleb James DeLisle <cjd@cjdns.fr>
2020-03-18 09:26:39 +0100

    Wire the search box

--- src/hooks/useBox.js
+++ src/hooks/useBox.js
@@ -1,13 +1,15 @@
 import { useState } from 'react'
+import { fetchJson } from '../utils'
+import nThen from 'nthen'
 import endpoints from '../utils/endpoints'
-const { addrMetaApi } = endpoints
+const { addrMetaApi, blkUpApi, blockApi, txApi } = endpoints
 
 const useBox = (histHook) => {
   const [inputs, setInputs] = useState({ omni: '' })
 
   const handleInputChange = (event) => {
     event.persist()
-    console.log(event)
+    //console.log(event)
     setInputs(inputs => ({ omni: event.target.value }))
   }
 
@@ -15,15 +17,69 @@ const useBox = (histHook) => {
     if (event) {
       event.preventDefault()
     }
-    const input = inputs.omni
-    try {
-      console.log('test addres')
-      fetch(`${addrMetaApi}/${input}`)
-        .then((json) => { // since there is no error we have adress metadata, we can link to the address
-          histHook.push(`/address/${input}`) // used to link to routes progamatically
-        })
-    } catch (error) {
-      console.log('this is not an adress')
+    let failed = false;
+    const error = (msg) => {
+      if (failed) { return }
+      failed = true;
+      // TODO(cjd): Jerome, can we have something a bit nicer, note how the old explorer works
+      alert(msg)
+    }
+    const input = ('' + inputs.omni).trim()
+    if (input === Number(input).toString()) {
+      const nIn = Number(input);
+      if (nIn < 0 || nIn !== Math.floor(nIn)) {
+        return void error('Block number must be a positive integer or zero')
+      } else {
+        // It's a block number
+        fetchJson(`${blkUpApi}/1/${Number(input) + 1}`).then((json) => {
+          if (json.code === 404) {
+            error(`Block #${input} not found`)
+          } else if (json.results && json.results.length === 1) {
+            histHook.push(`/block/${json.results[0].hash}`)
+          } else {
+            error(`Error requesting block #${input}`)
+          }
+        });
+      }
+    } else if (/[a-f0-9]{64}/.test(input)) {
+      // Hmm, it might be a block hash and then it might be a tx hash
+      // We make two requests, but in the next page load one of them will be useful
+      let res;
+      nThen((w) => {
+        fetchJson(`${blockApi}/${input}`).then(w((json) => {
+          if (json.code === 404) {
+          } else if (json.hash) {
+            res = 'block'
+          } else {
+            error(`Error requesting block with hash ${input}`)
+          }
+        }))
+        fetchJson(`${txApi}/${input}`).then(w((json) => {
+          if (json.code === 404) {
+          } else if (json.txid) {
+            res = 'tx'
+          } else {
+            error(`Error requesting transaction with hash ${input}`)
+          }
+        }))
+      }).nThen((w) => {
+        if (res) {
+          histHook.push(`/${res}/${input}`)
+        } else {
+          error(`No such block or transaction found`)
+        }
+      })
+    } else {
+      // None of the above, so it could be an address but we need to check
+      fetchJson(`${addrMetaApi}/${input}`).then((json) => {
+        if (json.code === 400) {
+          error(`${input} is not a valid address`)
+        } else if (typeof(json.balance) === 'string') {
+          histHook.push(`/address/${input}`)
+        } else {
+          error(`Error requesting address ${input}`)
+        }
+      });
     }
   }
 
