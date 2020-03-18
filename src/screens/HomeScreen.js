411456c73e817b8bc388a529478fcec771af0a00
gorhgorh <gorhgorh@lesnodebots.eu>
2020-03-18 13:46:00 +0100

    load more block function on the home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -3,32 +3,57 @@ import { fetchJson, treatStats, useInterval } from '../utils'
 import HomeStats from '../components/HomeStats/HomeStats'
 import endpoints from '../utils/endpoints'
 import BlockList from '../components/BlockList/BlockList.js'
+import { uniqBy } from 'lodash-es'
+import { Button } from '../components/CommonComps/CommonComps'
+import styled from 'styled-components'
 const { pkApi, blkDownApi, statsCoinsApi } = endpoints
+
+const BtRow = styled.div`
+  margin-top: 1rem;
+  text-align: center;
+`
 
 const HomeScreen = (props) => {
   const [pkData, setPkData] = useState(false)
+  const [currPage, setCurrPage] = useState(1)
   const [blockList, setBlockList] = useState(false)
   const [statsCoins, setStatsCoins] = useState(false)
   const [hasErr, setErr] = useState(false)
 
   if (hasErr) console.error(hasErr)
 
-  let topBlock = -1;
+  const topBlock = -1
   const loadBlocks = (then) => {
     // fetch blockList (first 20)
     fetchJson(`${blkDownApi}/20`)
       .then((json) => {
         if (json.error) { return void setErr(json.error) }
-        setBlockList(json.results)
+        if (blockList) setBlockList(uniqBy([...json.results, ...blockList], 'hash'))
+        else setBlockList(json.results)
       })
-    
-    fetchJson(`${statsCoinsApi}${ (topBlock >= 0) ? `/${topBlock}` : '' }`).then((json) => {
+
+    fetchJson(`${statsCoinsApi}${(topBlock >= 0) ? `/${topBlock}` : ''}`).then((json) => {
       setStatsCoins(json)
     })
   }
   useInterval(() => {
     loadBlocks()
   }, 30000)
+
+  const loadMoreBlock = () => {
+    console.log('moarrr clicked')
+    fetchJson(`${blkDownApi}/20/${currPage + 1}`)
+      .then((json) => {
+        if (json.error) setErr(json.error)
+        const newList = uniqBy([...blockList, ...json.results], 'hash')
+        console.log(newList)
+        setTimeout(() => {
+          window.scrollTo(0, document.body.scrollHeight)
+        }, 200) // TODO: (jerome) smooth scroll, no scroll ?
+        setBlockList(newList)
+        setCurrPage(currPage + 1)
+      })
+  }
 
   useEffect(() => {
     loadBlocks()
@@ -48,10 +73,12 @@ const HomeScreen = (props) => {
     fetchJson(`${pkApi}`)
   }, [])
 
-
   return <>
     <HomeStats txData={pkData} blockList={blockList} statsCoins={statsCoins} lastBlockData={{}} labelY='diffculty' />
     <BlockList listData={blockList} home/>
+    <BtRow>
+      <Button onClick={loadMoreBlock}>More blocks</Button>
+    </BtRow>
   </>
 }
 
